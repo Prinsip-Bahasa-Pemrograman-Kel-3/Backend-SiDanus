ca35479cd4750e784e1e34e277f67b6aee920353
fathuhu <fathan.khairun205@gmail.com>
2024-11-03 10:12:56 +0700

    save progress

---
+++ app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+    class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'stock',
+        'minimum_order',
+        'description',
+        'product_category_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Product_Category::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(Product_Image::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Product_Review::class);
+    }
+
+}
