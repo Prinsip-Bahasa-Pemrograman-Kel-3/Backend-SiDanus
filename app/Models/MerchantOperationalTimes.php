aeb16f9dd44a04bb64ab63aca7c990f596120d33
Astria Rizka <astria.rizka.tif423@polban.ac.id>
2024-11-08 10:33:55 +0700

    Menambahkan Models dan Controller Day dan MerchantOperationalTimes

---
+++ app/Models/MerchantOperationalTimes.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MerchantOperationalTimes extends Model
+{
+    use HasFactory;
+
+    protected $table = 'merchant_operational_times';
+
+    protected $fillable = [
+        'day_id',
+        'merchant_id',
+    ];
+
+    // Relasi ke model Days
+    public function day()
+    {
+        return $this->belongsTo(Days::class, 'day_id');
+    }
+
+    // Relasi ke model Merchants
+    public function merchant()
+    {
+        return $this->belongsTo(Merchants::class, 'merchant_id');
+    }
+}
